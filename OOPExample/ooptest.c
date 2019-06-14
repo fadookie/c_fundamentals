d2f488f1b9b35a1a46c7b5164c16fc909354c316
Eliot Lash <fadookie@gmail.com>
2019-06-14 11:16:11 -0700

    static struct copy demo

--- OOPExample/ooptest.c
+++ OOPExample/ooptest.c
@@ -12,18 +12,61 @@ typedef struct {
     int count;
 } inventoryItem;
 
-void printStruct (inventoryItem this) {
-    printf("inventoryItem { id:'%s' count:'%i' }\n", this.id, this.count);
+typedef struct {
+    const char* id;
+    inventoryItem inventory[2];
+} inventoryGroup;
+
+void printItem (inventoryItem this) {
+    printf("inventoryItem { id:'%s' count:'%i' }", this.id, this.count);
+}
+
+void printGroup (inventoryGroup this) {
+    printf("inventoryGroup { id:'%s' inventory: [\n", this.id);
+    for (int i = 0; i < sizeof(this.inventory) / sizeof(inventoryItem); ++i) {
+        printf("  ");
+        printItem(this.inventory[i]);
+        printf(",\n");
+    }
+    printf("]}\n");
+}
+
+void printGroups(inventoryGroup* groups, const int numGroups) {
+    for (int groupIdx = 0; groupIdx < numGroups; ++groupIdx) {
+        inventoryGroup group = groups[groupIdx];
+        printGroup(group);
+    }
 }
 
 void doOopTest() {
     puts("do OOP test");
     
-    inventoryItem inventory[] = {
-        { .id = "apples", .count = 5 },
-        { .id = "bananas", .count = 3 }
+    inventoryGroup groups[] = {
+        {
+            .id = "fruits",
+            .inventory = {
+                { .id = "apples", .count = 5 },
+                { .id = "bananas", .count = 0 }
+            }
+        },
+        {
+            .id = "vegetables",
+            .inventory = {
+                { .id = "potatoes", .count = 4 },
+                { .id = "carrots", .count = 2 }
+            }
+        },
     };
-    for (int i = 0; i < sizeof(inventory) / sizeof(inventoryItem); ++i) {
-        printStruct(inventory[i]);
-    }
+    const int numGroups = sizeof(groups) / sizeof(inventoryGroup);
+    
+    puts("Initial values:");
+    printGroups(groups, numGroups);
+    
+    puts("\nCopy of fruit:");
+    inventoryGroup groupCopy = groups[0];
+    ++groupCopy.inventory[1].count;
+    printGroup(groupCopy);
+    
+    puts("New values:");
+    printGroups(groups, numGroups);
 }
